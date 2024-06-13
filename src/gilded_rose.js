82c1806e295196f8d91fb0cdb732b2f06189ea0f
Felix Schober <if23b087@technikum-wien.at>
2024-06-13 07:49:29 +0200

    14. test green

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -14,38 +14,38 @@ items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20));
 items.push(new Item('Conjured Mana Cake', 3, 6));
 
 function update_quality() {
-  items.forEach(item => {
+  let qualityChange = -1
+  let sellInChange = -1
+  let qualityMax = 50
+  let degradationMultiplierAfterExpired = 2
 
-    let qualityChange = -1
-    let sellInChange = -1
-    let qualityMax = 50
-    let degradationMultiplierAfterExpired = 2
-
-    let agedBrie = {
-      name: 'Aged Brie',
-      qualityChange: 1
-    }
-    let sulfuras = {
-      name: 'Sulfuras, Hand of Ragnaros',
-      qualityChange: 0,
-      sellInChange: 0
-    }
-    let conjured = {
-      name: 'Conjured Mana Cake',
-      degradationMultiplier: 2
-    }
-    let backstagePasses = {
+  let agedBrie = {
+    name: 'Aged Brie',
+    qualityChange: 1
+  }
+  let sulfuras = {
+    name: 'Sulfuras, Hand of Ragnaros',
+    qualityChange: 0,
+    sellInChange: 0
+  }
+  let conjured = {
+    name: 'Conjured',
+    degradationMultiplier: 2
+  }
+  let backstagePasses = {
       name: 'Backstage passes to a TAFKAL80ETC concert',
       defaultQualityChange: 1,
       sell_in: [5, 10],
       qualityIncrease: [3, 2]
-    }
+  }
+  
+  items.forEach(item => {
 
-    switch (item.name){
-      case agedBrie.name:
+    switch (true){
+      case item.name.startsWith(agedBrie.name):
         qualityChange = agedBrie.qualityChange
         break;
-      case backstagePasses.name:
+      case item.name.startsWith(backstagePasses.name):
         qualityChange = backstagePasses.defaultQualityChange
         if (item.sell_in <= 1){
           item.quality = 0
@@ -59,11 +59,11 @@ function update_quality() {
           }
         }
         break;
-      case sulfuras.name:
+      case item.name.startsWith(sulfuras.name):
         qualityChange = sulfuras.qualityChange
         sellInChange = sulfuras.sellInChange
         break;
-      case conjured.name:
+      case item.name.startsWith(conjured.name):
         qualityChange *= conjured.degradationMultiplier
         break
       default:
@@ -71,7 +71,7 @@ function update_quality() {
           qualityChange *= degradationMultiplierAfterExpired
         }
     }
-    
+
     item.sell_in += sellInChange
     item.quality += qualityChange
     if (item.quality < 0){
