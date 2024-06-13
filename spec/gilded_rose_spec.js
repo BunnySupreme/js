7c01e2106b59a8d4ad7f1b44340d5f217c832b04
Felix Schober <if23b087@technikum-wien.at>
2024-06-13 03:55:02 +0200

    7. test green (had to change Sulfuras to the full name, Sulfuras, Hand of Ragnaros. I assume the item should always be named like that)

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -36,4 +36,10 @@ describe("Gilded Rose", function() {
     expect(items[0].quality).toEqual(50);
   });
 
+  it("should not decrease sell by date of Sulfuras", function() {
+    items = [ new Item("Sulfuras, Hand of Ragnaros", 1, 0) ];
+    update_quality();
+    expect(items[0].sell_in).toEqual(1);
+  });
+
 });
