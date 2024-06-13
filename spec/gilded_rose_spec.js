ad18127432f1bc993c24a4617e1bf3a3a7af9d8e
Felix Schober <if23b087@technikum-wien.at>
2024-06-13 03:50:09 +0200

    2. test green

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -6,4 +6,10 @@ describe("Gilded Rose", function() {
     expect(items[0].sell_in).toEqual(-1);
   });
 
+  it("should lower Quality", function() {
+    items = [ new Item("normalItem", 0, 1) ];
+    update_quality();
+    expect(items[0].quality).toEqual(0);
+  });
+
 });
